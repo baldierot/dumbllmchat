155fb998dec503fe00c1e0fca8ccdfc8479423ae
Baldie <baldierot@gmail.com>
2025-07-19 16:36:43 +0300

    pwa

---
+++ public/sw.js
@@ -0,0 +1,33 @@
+
+const CACHE_NAME = 'dumbllmchat-cache-v1';
+const urlsToCache = [
+  '/',
+  '/css/style.css',
+  '/js/api.js',
+  '/js/app.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).then(response => {
+      const responseToCache = response.clone();
+      caches.open(CACHE_NAME).then(cache => {
+        cache.put(event.request, responseToCache);
+      });
+      return response;
+    }).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
