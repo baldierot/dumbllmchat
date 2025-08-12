867ff91e5f8ce27044212e8bb48dfd80f30851e0
baldierot <baldierot@gmail.com>
2025-08-12 13:30:33 +0300

    Updates

---
+++ sw.js
@@ -0,0 +1,39 @@
+
+const CACHE_NAME = 'dumbllmchat-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/api.js',
+  '/js/app.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // If the request was successful, cache the response and return it
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+        return response;
+      })
+      .catch(() => {
+        // If the network request fails, try to find a match in the cache
+        return caches.match(event.request);
+      })
+  );
+});
