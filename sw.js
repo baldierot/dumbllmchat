e9107666572806e08ffea1a002ac4dacaede3223
baldierot <baldierot@gmail.com>
2025-10-19 17:07:57 +0300

    Updates

--- sw.js
+++ sw.js
@@ -81,14 +81,14 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "url": "assets/index-BqKY8Qir.css",
     "revision": null
   }, {
-    "url": "assets/index-BsOvN5yI.js",
+    "url": "assets/index-mmRRdWQ7.js",
     "revision": null
   }, {
     "url": "images/icon-512x512.png",
     "revision": "bb3850071170a4b0d1c31e4adb1896b5"
   }, {
     "url": "index.html",
-    "revision": "6742b6cafa82a699772b5ff6ac94ffa7"
+    "revision": "3987848bfbe8dfc7b7b1800391365649"
   }, {
     "url": "registerSW.js",
     "revision": "6e18d96a85da70990010526b4d21040a"
