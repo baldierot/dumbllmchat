c88fd19e2ec5b363e86243bfcd7f0c223e52673f
baldierot <baldierot@gmail.com>
2025-10-19 16:07:47 +0300

    Updates

--- sw.js
+++ sw.js
@@ -78,17 +78,17 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/index--OD_1Crl.js",
+    "url": "assets/index-BqKY8Qir.css",
     "revision": null
   }, {
-    "url": "assets/index-BqKY8Qir.css",
+    "url": "assets/index-M1qTjEGf.js",
     "revision": null
   }, {
     "url": "images/icon-512x512.png",
     "revision": "bb3850071170a4b0d1c31e4adb1896b5"
   }, {
     "url": "index.html",
-    "revision": "0a95644d3d72554634a66736826a38f3"
+    "revision": "c6402ce141f512541ea9df5f55c99160"
   }, {
     "url": "registerSW.js",
     "revision": "6e18d96a85da70990010526b4d21040a"
